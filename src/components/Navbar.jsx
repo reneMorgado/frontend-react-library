be49446a5168e3c2dc289bdf208c41c394860877
Rene Morgado <renemorgado33@gmail.com>
2021-12-02 23:42:42 -0600

    fix(js): try to fix empty page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { NavLink, useHistory } from 'react-router-dom'
-import { userContext, useState } from '../context/userContext'
+import { userContext } from '../context/userContext'
 
 const Navbar = () => {
 
